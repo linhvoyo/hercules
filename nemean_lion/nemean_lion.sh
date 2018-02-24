eab01f3702bc2274641bc8bb0b0c34bc6284716f
Linh Lam <lilam@e1z2r9p41.42.us.org>
2018-02-23 17:12:36 -0800

    edit port

--- nemean_lion/nemean_lion.sh
+++ nemean_lion/nemean_lion.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 apt-get -y update && apt-get install -y openssh-server
-sed -i.backup s'/#Port 22/Port 655535/' /etc/ssh/sshd_config
+sed -i.backup s'/#Port 22/Port 65535/' /etc/ssh/sshd_config
 service ssh restart
